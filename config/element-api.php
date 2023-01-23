d55ec2839880e2190f2bdba6c554f44718bce6f5
Ronan <roleary81@gmail.com>
2023-01-23 20:48:07 +0100

    add studio endpoint for the moment

--- config/element-api.php
+++ config/element-api.php
@@ -20,6 +20,38 @@ return [
                 },
             ];
         },
+        'api/studio.json' => function() {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['section' => 'studio', 'limit' => 1],
+                'transformer' => function(Entry $entry) {
+
+                    $panels = [];
+                    foreach ($entry->studioIndustryExpPanels as $row){
+                        $panels[] = [
+                            'subTitle' => $row->subtitle,
+                            'industryTitle' => $row->industryTitle,
+                            'industryBody' => $row->industryBody
+                        ];
+                    };
+
+                    return [
+                        'metaTitle' => $entry->metaTitle,
+                        'metaDescription' => $entry->metaDescription,
+                        'metaImage' => $entry->metaImage,
+                        'studioEyebrowText' => $entry->studioEyebrowText,
+                        'studioTitle' => $entry->studioTitle,
+                        'studioIntro' => $entry->studioIntro,
+                        'studioCta' => $entry->studioCta,
+                        'studioIndustryExpTitle' => $entry->studioIndustryExpTitle,
+                        'studioIndustryExpIntro' => $entry->studioIndustryExpIntro,
+                        'studioIndustryExpPanels' => $panels,
+                        'url' => $entry->url,
+                        'jsonUrl' => UrlHelper::url("api/studio.json")
+                    ];
+                },
+            ];
+        },
        
     ]
 ];
