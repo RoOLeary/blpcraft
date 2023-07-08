f3f00e668e3ca92273a3e225b9c9a5cba279c5c1
Ronan <roleary81@gmail.com>
2023-07-08 21:43:37 +0200

    and another

--- config/element-api.php
+++ config/element-api.php
@@ -306,7 +306,7 @@ return [
                             'articleTitle' => $entry->articleTitle,
                             'articleTypePostDate' => $entry->postDate->format(\DateTime::ATOM),
                             'articleExcerpt' => $entry->articleExcerpt,
-                            'articleCategories' => $category, 
+                            'articleCategories' => $entry->articleCategories, 
                             'articleFeaturedImage' => $entry->articleFeaturedImage,
                             'articleImageUrl' => $entry->articleImageUrl,
                             'articleImageAlt' => $entry->articleImageAlt,
